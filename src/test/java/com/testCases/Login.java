3e3936d3634bb7207f0938f150b6c5399224df94
nileshdhale <nilesh.rkr@gmail.com>
2026-05-31 13:44:56 +0530

    remove deleted files

--- src/test/java/com/testCases/Login.java
+++ src/test/java/com/testCases/Login.java
@@ -1,10 +1,4 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 import java.time.Duration;
 
